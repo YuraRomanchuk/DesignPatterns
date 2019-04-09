ff6618ef915654567df160eac5276f817e6494ae
YuraRomanchuk <33698979+YuraRomanchuk@users.noreply.github.com>
2019-04-09 15:04:37 +0300

    Add facade pattern

---
+++ DesignPatterns/Facade/Facade.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Facade
+{
+    class Facade
+    {
+        private SubClass1 subClass1;
+
+        private SubClass2 subClass2;
+
+        public Facade(SubClass1 subClass1, SubClass2 subClass2)
+        {
+            this.subClass1 = subClass1;
+            this.subClass2 = subClass2;
+        }
+
+        public void Operation()
+        {
+            this.subClass1.Operation();
+        }
+    }
+
+    class SubClass1
+    {
+        public string Operation()
+        {
+            return "oper1";
+        }
+    }
+
+    class SubClass2
+    {
+        public string Operation()
+        {
+            return "oper2";
+        }
+    }
+
+    class Client
+    {
+        public void Operation(Facade facade)
+        {
+            facade.Operation();
+        }
+    }
+}
